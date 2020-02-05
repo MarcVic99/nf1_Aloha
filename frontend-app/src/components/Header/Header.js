be4875b85e7b972c0530689fc42a661c20fd3e7c
Mostapha <mostapha-ach@hotmail.com>
2020-02-05 21:17:17 +0100

    Mustapha Last Commit

--- frontend-app/src/components/Header/Header.js
+++ frontend-app/src/components/Header/Header.js
@@ -17,26 +17,14 @@ const Header = () => {
     useEffect(() => {
         fetch('http://127.0.0.1:80//api/search/city/barcelona')
             .then(response => response.json())
-            .then(payload => {
-                if (payload.status === 'succes') {
-                    dispatch({
-                        type: 'SEARCH_PROPERTIES_SUCCESS',
-                        payload: payload.properties
-                    });
-                } else {
-                    dispatch({
-                        type: 'SEARCH_PROPERTIES_FAILURE',
-                        error: payload
-                    });
-                }
-            });
+
 
     }, [])
 
 
     let retrievedProperties = <div/>;
     if (loading && !errorMessage) {
-        retrievedProperties = <img className="spinner" src={spinner} alt="Loading spinner"/>;
+       // retrievedProperties = <img className="spinner" src={spinner} alt="Loading spinner"/>;
     } else if (errorMessage) {
         retrievedProperties = <div className="errorMessage">{errorMessage}</div>;
     } else if (properties) {
